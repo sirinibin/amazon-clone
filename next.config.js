059cda7578d8dbb6b559bc23a6c88dc5e99eae32
sirin kolasseri <sirin@sirins-MacBook-Air.local>
2024-05-11 17:40:09 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+        domains: ["links.papareact.com","fakestoreapi.com","encrypted-tbn0.gstatic.com"]
+    }
+}
---
+++ src/components/Product.js
@@ -0,0 +1,43 @@
+import Image from "next/image"
+import { useState } from "react"
+import { StarIcon } from "@heroicons/react/solid"
+import Currency from "react-currency-formatter"
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
+
+function Product({ id, title, price, description, category, image }) {
+
+    const [rating] = useState(
+        Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+    );
+
+    const [hasPrime] = useState(Math.random() < 0.5)
+
+    return (
+        <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+            <p className="absolute top-2 right-2 text-xs italic">{category}</p>
+            <Image src={image} height={200} width={200} objectFit="contain" alt="" />
+            <h4 className="my-3">{title}</h4>
+            <div className="flex">
+                {Array(rating).fill().map((_, i) => (
+                    <StarIcon className="h-5 text-yellow-500" />
+                ))}
+            </div>
+            <p className="text-xs my-2 line-clamp-2">{description}</p>
+            <div className="mb-5">
+                <Currency quantity={price} currency="GBP" />
+            </div>
+            {hasPrime && (
+                <div className="flex items-center space-x-2 -mt-5">
+                    <img  className="w-12" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpuL2TSBc2PL_Z-1rjoRC9KTNeVUeS-eHz5R-8MlNFFw&s" alt="" />
+                    <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+                </div>
+            )}
+          
+           <button className="mt-auto button"> Add to Basket</button>
+        </div>
+    )
+
+}
+export default Product
